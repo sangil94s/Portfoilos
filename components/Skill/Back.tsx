953947e698b225a1fa0dd021bc5950d9f018d4ed
한상일 <hansang-il@MacBookAir.localdomain>
2025-07-07 11:10:26 +0900

    ADD : 자기 소개 부분에 타이핑 애니메이션 추가

--- components/Skill/Back.tsx
+++ components/Skill/Back.tsx
@@ -8,7 +8,7 @@ export default function Back() {
       <div className="mx-2">
         <h1 className="text-center text-xl font-bold py-2">Backend</h1>
         <div className="grid grid-cols-1 gap-2 justify-items-center lg:grid-cols-4">
-          <section className="border border-slate-300 rounded-md m-2 w-full hover:bg-gray-100 hover:shadow-md transition-all duration-200">
+          <section className="border border-slate-300 rounded-md m-2 w-full max-w-sm hover:bg-gray-100 hover:shadow-md transition-all duration-200">
             <div className="flex flx-row m-2">
               <Image
                 src={'/dae.webp'}
